ab28fd72af3085c53da4825f4cd8fab0bfe31666
max2556 <46759632+max2556@users.noreply.github.com>
2023-11-10 20:07:53 +0300

    🩹 OurSkillsBlock: new text content

--- src/pages/landing/ourSkillsBlock/ui/skills.ts
+++ src/pages/landing/ourSkillsBlock/ui/skills.ts
@@ -15,45 +15,57 @@ export const skillsDesktop: (Skill & {
     x: 2,
     y: 1,
     image: cpp,
-    description: 'stl, opengl, libcurl, json, googletest, tinyXML2',
+    description: 'STL, Boost, Qt, OpenGL, libcurl, Json, GoogleTest'
   },
-  { x: 6, y: 2, image: cSharp, line: 'rb', description: '.net 6 core' },
-  { x: 10, y: 1, image: c, line: 'rt', description: 'opengl' },
+  {
+    x: 6,
+    y: 2,
+    image: cSharp,
+    line: 'rb',
+    description: '.Net6/7, Avalonya, .NET framework'
+  },
+  {
+    x: 10,
+    y: 1,
+    image: c,
+    line: 'rt',
+    description: 'OpenGL, Low-level programming'
+  },
   {
     x: 11,
     y: 5,
     image: linux,
     line: 'br',
-    description: 'bash, Command Line Interface',
+    description: 'Bash, Command Line Interface'
   },
   {
     x: 1,
     y: 7,
     image: python,
     line: 'lb',
-    description: 'Flask, django, pandas, opencv, grpc, telethon',
+    description: 'Flask, Django, Pandas, Opencv, gRPC, Numpy'
   },
   {
     x: 5,
     y: 6,
     image: java,
     line: 'bl',
-    description: 'core advanced, spring framework',
+    description: 'Core Advanced, Spring, Hibernate, JDBC, MyBatis'
   },
   {
     x: 9,
     y: 7,
     image: office,
     line: 'rb',
-    description: 'Word, Excel, PowerPoint, Access, Publisher',
+    description: 'Word, Excel, PowerPoint, Access, Publisher'
   },
   {
     x: 6,
     y: 10,
     image: react,
     line: 'br',
-    description: 'React, JavaScript, HTML5, CSS3',
-  },
+    description: 'React, JavaScript frameworks, HTML5, CSS3'
+  }
 ];
 
 export const skillsMobile: (Skill & {
@@ -63,43 +75,55 @@ export const skillsMobile: (Skill & {
     x: 5,
     y: 1,
     image: cpp,
-    description: 'stl, opengl, libcurl, json, googletest, tinyXML2',
+    description: 'STL, Boost, Qt, OpenGL, libcurl, Json, GoogleTest'
   },
-  { x: 1, y: 2, image: cSharp, line: 'lb', description: '.net 6 core' },
-  { x: 2, y: 6, image: c, line: 'br', description: 'opengl' },
+  {
+    x: 1,
+    y: 2,
+    image: cSharp,
+    line: 'lb',
+    description: '.Net6/7, Avalonya, .NET framework'
+  },
+  {
+    x: 2,
+    y: 6,
+    image: c,
+    line: 'br',
+    description: 'OpenGL, Low-level programming'
+  },
   {
     x: 6,
     y: 5,
     image: linux,
     line: 'br',
-    description: 'bash, Command Line Interface',
+    description: 'Bash, Command Line Interface'
   },
   {
     x: 5,
     y: 9,
     image: python,
     line: 'bl',
-    description: 'Flask, django, pandas, opencv, grpc, telethon',
+    description: 'Flask, Django, Pandas, Opencv, gRPC, Numpy'
   },
   {
     x: 1,
     y: 10,
     image: java,
     line: 'lb',
-    description: 'core advanced, spring framework',
+    description: 'Core Advanced, Spring, Hibernate, JDBC, MyBatis'
   },
   {
     x: 2,
     y: 14,
     image: office,
     line: 'br',
-    description: 'Word, Excel, PowerPoint, Access, Publisher',
+    description: 'Word, Excel, PowerPoint, Access, Publisher'
   },
   {
     x: 6,
     y: 13,
     image: react,
     line: 'br',
-    description: 'React, JavaScript, HTML5, CSS3',
-  },
+    description: 'React, JavaScript frameworks, HTML5, CSS3'
+  }
 ];
